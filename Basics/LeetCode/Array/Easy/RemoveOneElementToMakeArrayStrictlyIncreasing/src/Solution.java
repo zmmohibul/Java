3931640d9c73f3ad36e996ca6f4a0d0fd853daaf
zmmohibul <18-36427-1@student.aiub.edu>
2022-07-30 16:53:22 +0600

    LeetCode - Remove One Element to Make the Array Strictly Increasing - Almost - 2

--- Basics/LeetCode/Array/Easy/RemoveOneElementToMakeArrayStrictlyIncreasing/src/Solution.java
+++ Basics/LeetCode/Array/Easy/RemoveOneElementToMakeArrayStrictlyIncreasing/src/Solution.java
@@ -6,29 +6,19 @@ public class Solution {
 
         int count = 0;
         int i = 0;
-        int j = 1;
-        int k = 2;
-        while (j < nums.length) {
-            if (k < nums.length) {
-                if (nums[j] <= nums[i] || nums[j] >= nums[k]) {
-                    count += 1;
-                    nums[j] = nums[i];
-                }
-            } else {
-                if (nums[j] <= nums[i]) {
-                    count += 1;
-                }
+        while (i < nums.length - 1) {
+            if (nums[i] > nums[i + 1]) {
+                count += 1;
+                i += 1;
             }
 
             if (count > 1) {
-                return  false;
+                return false;
             }
 
             i += 1;
-            j += 1;
-            k += 1;
         }
 
-        return true;
+        return  true;
     }
 }
